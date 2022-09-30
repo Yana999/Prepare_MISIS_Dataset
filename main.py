8c55d54a827d399fd2d1a9902b639e29dca5ad42
TheNoneMan <bikos1999@gmail.com>
2022-10-01 01:41:17 +0300

    Concurrent YT download

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 # importing libraries
+from __future__ import unicode_literals
+
+from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
 import speech_recognition as sr
+import yt_dlp
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 from tqdm import tqdm
 
-
 CHUNKS_FOLDER = Path(r'./Audio_chunks')
 TRANSCRIPTS_FOLDER = Path(r'./Transcripts')
+
+CHUNKS_FOLDER.mkdir(exist_ok=True)
+TRANSCRIPTS_FOLDER.mkdir(exist_ok=True)
 
 # a function that splits the audio file into chunks
 # and applies speech recognition
 def silence_based_conversion(path):
+
+    audio_subfolder = CHUNKS_FOLDER / path.name
+    transcripts_subfolder = TRANSCRIPTS_FOLDER / path.name
+
+    audio_subfolder.mkdir(exist_ok=True)
+    transcripts_subfolder.mkdir(exist_ok=True)
     # open the audio file stored in
     # the local system as a wav file.
     song = AudioSegment.from_wav(path)
@@ -29,10 +41,10 @@ def silence_based_conversion(path):
                           silence_thresh=-20
                           )
     # create a directory to store the audio chunks.
-    CHUNKS_FOLDER.mkdir(exist_ok=True)
+
 
     # process each chunk
-    pbar = tqdm(chunks[1:], colour='green', unit='chunk', ncols=80)
+    pbar = tqdm(chunks[1:], colour='green', unit='chunk', ncols=80, desc=path.name)
     for i, chunk in enumerate(pbar):
         # Create 0.5 seconds silence chunk
         chunk_silent = AudioSegment.silent(duration=10)
@@ -43,11 +55,9 @@ def silence_based_conversion(path):
         audio_chunk = chunk_silent + chunk + chunk_silent
 
         # export audio chunk and save it in the current directory.
-        pbar.set_description_str('Saving')
-        chunk_file = CHUNKS_FOLDER / f"chunk_{i}.wav"
+        chunk_file = audio_subfolder / f"chunk_{i}.wav"
         # specify the bitrate to be 192 k
         audio_chunk.export(chunk_file, bitrate='192k', format="wav")
-        pbar.set_description_str('Processing')
 
         # create a speech recognition object
         r = sr.Recognizer()
@@ -60,16 +70,34 @@ def silence_based_conversion(path):
                 # try converting it to text
                 rec = r.recognize_google(audio, language="ru-RU")
                 # write the output to the file.
-                TRANSCRIPTS_FOLDER.mkdir(exist_ok=True)
-                with (TRANSCRIPTS_FOLDER / f'recognized_{i}.txt').open('w+', encoding='utf-8') as f:
+                with (transcripts_subfolder / f'recognized_{i}.txt').open('w+', encoding='utf-8') as f:
                     f.write(rec)
                 # catch any errors.
-            except sr.UnknownValueError:
-                print("Could not understand audio")
-            except sr.RequestError:
-                print("Could not request results. check your internet connection")
+            except Exception:
+                pass
+            # except sr.UnknownValueError:
+            #     pbar.write(f"{i}: Could not understand audio")
+            # except sr.RequestError:
+            #     pbar.write(f"{i}: Could not request results. check your internet connection")
 
 if __name__ == '__main__':
-    source_audio = Path(r"./audio/rec1.2.wav").resolve()
-    print(f'Audio file path: {source_audio}')
-    silence_based_conversion(source_audio)
+
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': './audio/YT_video_%(autonumber)d.%(ext)s',
+    'quiet': True,
+    'concurrent_fragment_downloads': 3,
+    'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '192',
+        }],
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        with open(r'./youtube.txt', encoding='utf-8') as f:
+            ydl.download(f.read().splitlines())
+
+
+    audio_folder = Path(r"./audio")
+    with ProcessPoolExecutor(max_workers=3) as executor:
+        executor.map(silence_based_conversion, audio_folder.iterdir())
