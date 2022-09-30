5bdeb9604348287123d30ef122400515c770178b
TheNoneMan <bikos1999@gmail.com>
2022-10-01 00:28:04 +0300

    Prettify

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # importing libraries
-import os
+from pathlib import Path
 
 import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
+from tqdm import tqdm
 
+
+CHUNKS_FOLDER = Path(r'./Audio_chunks')
+TRANSCRIPTS_FOLDER = Path(r'./Transcripts')
 
 # a function that splits the audio file into chunks
 # and applies speech recognition
-def silence_based_conversion(path=r"C:\Users\Dns\voice\rec1.2.wav"):
+def silence_based_conversion(path):
     # open the audio file stored in
     # the local system as a wav file.
     song = AudioSegment.from_wav(path)
-    # open a file where we will concatenate
-    # and store the recognized text
-    fh = open("recognized.txt", "w+")
     # split track where silence is 0.5 seconds
     # or more and get chunks
     chunks = split_on_silence(song,
@@ -25,58 +26,50 @@ def silence_based_conversion(path=r"C:\Users\Dns\voice\rec1.2.wav"):
                           min_silence_len=700,
                           # consider it silent if quieter than -16 dBFS
                           # adjust this per requirement
-                          silence_thresh=- 20
+                          silence_thresh=-20
                           )
     # create a directory to store the audio chunks.
-    try:
-        os.mkdir('audio_chunks')
-    except (FileExistsError):
-        pass
-    # move into the directory to
-    # store the audio files.
-    os.chdir('audio_chunks')
+    CHUNKS_FOLDER.mkdir(exist_ok=True)
+
     # process each chunk
-    for i, chunk in enumerate(chunks):
+    pbar = tqdm(chunks[1:], colour='green', unit='chunk', ncols=80)
+    for i, chunk in enumerate(pbar):
         # Create 0.5 seconds silence chunk
         chunk_silent = AudioSegment.silent(duration=10)
+
         # add 0.5 sec silence to beginning and
         # end of audio chunk. This is done so that
         # it doesn't seem abruptly sliced.
         audio_chunk = chunk_silent + chunk + chunk_silent
-        # export audio chunk and save it in
-        # the current directory.
-        print(f"saving chunk{i}.wav")
+
+        # export audio chunk and save it in the current directory.
+        pbar.set_description_str('Saving')
+        chunk_file = CHUNKS_FOLDER / f"chunk_{i}.wav"
         # specify the bitrate to be 192 k
-        audio_chunk.export(f"./chunk {i}.wav", bitrate='192k', format="wav")
-        # the name of the newly created chunk
-        filename = f'chunk {i}.wav'
-        print(f"Processing chunk {i}")
-        # get the name of the newly created chunk
-        # in the AUDIO_FILE variable for later use.
+        audio_chunk.export(chunk_file, bitrate='192k', format="wav")
+        pbar.set_description_str('Processing')
+
         # create a speech recognition object
         r = sr.Recognizer()
         # recognize the chunk
-        with sr.AudioFile(filename) as source:
-            # remove this if it is not working
-            # correctly.
-            r.dynamic_energy_threshold = True
+        with sr.AudioFile(str(chunk_file)) as source:
+            # remove this if it is not working correctly.
             r.adjust_for_ambient_noise(source)
             audio = r.record(source)
             try:
                 # try converting it to text
                 rec = r.recognize_google(audio, language="ru-RU")
                 # write the output to the file.
-                with open(f'../recognized/recognized {i}.txt', 'w+') as f:
+                TRANSCRIPTS_FOLDER.mkdir(exist_ok=True)
+                with (TRANSCRIPTS_FOLDER / f'recognized_{i}.txt').open('w+', encoding='utf-8') as f:
                     f.write(rec)
                 # catch any errors.
             except sr.UnknownValueError:
                 print("Could not understand audio")
-            except sr.RequestError as e:
+            except sr.RequestError:
                 print("Could not request results. check your internet connection")
-            i += 1
-        os.chdir('..')
 
 if __name__ == '__main__':
-    print('Enter the audio file path')
-    # path = input()
-    silence_based_conversion()
+    source_audio = Path(r"./audio/rec1.2.wav").resolve()
+    print(f'Audio file path: {source_audio}')
+    silence_based_conversion(source_audio)
